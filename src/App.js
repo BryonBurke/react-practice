6c08dccdb8c03823781d94aee3cf261be24edf46
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-03-30 12:11:01 -0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    function formatDate(date) {
+      return date.toLocaleDateString();
+    }
+
+    function Avatar(props) {
+      return (
+        <img
+          className="Avatar"
+          src={props.user.avatarUrl}
+          alt={props.user.name}
+        />
+      );
+    }
+
+    function UserInfo(props) {
+      return (
+        <div className="UserInfo">
+          <Avatar user={props.user} />
+          <div className="UserInfo-name">{props.user.name}</div>
+        </div>
+      );
+    }
+
+    function Comment(props) {
+      return (
+        <div className="Comment">
+          <UserInfo user={props.author} />
+          <div className="Comment-text">{props.text}</div>
+          <div className="Comment-date">
+            {formatDate(props.date)}
+          </div>
+        </div>
+      );
+    }
+
+    const comment = {
+      date: new Date(),
+      text: 'I hope you enjoy learning    React!',
+      author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64',
+      },
+    };
+    ReactDOM.render(
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+      />,
+      document.getElementById('root')
+    );
+
   );
 }
 
